a197787b5fb3a522ab00884d174fe1f40f5123bb
Kado Damball <kadodamball@hotmail.com>
2014-06-26 02:53:26 +0300

    Passing the final test but it's not solving the puzzles

--- src/solve.js
+++ src/solve.js
@@ -19,7 +19,7 @@ function saveEmptyPositions(brd){
 };
 
 function checkRow(brd, row, val){
-  for(var i = 0; i < brd[row].length; ){
+  for(var i = 0; i < brd[row].length; i++){
     if(brd[row][i] === val){return false;}
   }
   
@@ -64,40 +64,49 @@ function checkValue(brd, row, column, val){
   }
 }
 
-function solvePuzzle(brd, emptyPositions){
+function solvePuzzle(board, emptyPositions) {
+  // Variables to track our position in the solver
   var limit = 9,
-    i, row, column, val, found;
-    
-  for(i = 0; i < emptyPositions.length; i++){
+      i, row, column, value, found;
+  for(i = 0; i < emptyPositions.length; i++) {
     row = emptyPositions[i][0];
     column = emptyPositions[i][1];
-    
-    val = brd[row][column] + 1;
-    
-    while(!found && val <= limit){
-    
-      if(this.checkValue(brd, row, column, val)){
-        found = true; //stop the loop and set our value
-        brd[row][column] = val;
+    // Try the next value
+    value = board[row][column] + 1;
+    // Was a valid number found?
+    found = false;
+    // Keep trying new values until either the limit
+    // was reached or a valid value was found
+    while(!found && value <= limit) {
+      // If a valid value is found, mark found true,
+      // set the position to the value, and move to the
+      // next position
+      if(this.checkValue(board, column, row, value)) {
+        found = true;
+        board[row][column] = value;
         i++;
-      }else{
-        val++;
+      } 
+      // Otherwise, try the next value
+      else {
+        value++;
       }
-      
     }
-    
-    if(!found){
-      brd[row][column] = 0;
-      --i;
+    // If no valid value was found and the limit was
+    // reached, move back to the previous position
+    if(!found) {
+      board[row][column] = 0;
+      i--;
     }
   }
-  
-  brd.forEach(function(row){
+
+  // A solution was found! Log it
+  board.forEach(function(row) {
     console.log(row.join());
   });
-  
-  return brd;
-}
+
+  // return the solution
+  return board;
+};
 
 module.exports = {
   parseBoard: parseBoard,
--- tests/test.js
+++ tests/test.js
@@ -55,9 +55,56 @@ describe("Sudoku Solver", function(){
   
   describe("#checkRow()", function(){
     it("Should check that each value in the row does not equal the input", function(){
-      // no match, return true
+      // match, return false
       expect(solver.checkRow(parsedBoard, 0, 9)).to.not.be.ok;
+      // no match, return true
       expect(solver.checkRow(parsedBoard, 0, 5)).to.be.ok;
+    });
+  });
+  
+  describe("#checkColumns", function(){
+    it("Should check that each value in the column does not equal input", function(){
+      expect(solver.checkColumn(parsedBoard, 0, 9)).to.be.ok;
+      expect(solver.checkColumn(parsedBoard, 0, 5)).to.not.be.ok;
+    });
+  });
+  
+  describe("#check3x3Square", function(){
+    it("should check that each value in a 3x3 square doesn't match the input", function(){
+      // No match. Return true
+      expect(solver.check3x3Square(parsedBoard, 2, 2, 1)).to.be.ok;
+      expect(solver.check3x3Square(parsedBoard, 7, 7, 9)).to.be.ok;
+      // Match found. Return false
+      expect(solver.check3x3Square(parsedBoard, 2, 2, 9)).to.not.be.ok;
+      expect(solver.check3x3Square(parsedBoard, 7, 7, 1)).to.not.be.ok;
+    });
+  });
+  
+  describe("#checkValue()", function(){
+    it("should check whether a value is valid for a particular position", function(){
+      // No match. Return true
+      expect(solver.checkValue(parsedBoard, 0, 0, 2)).to.be.ok;
+      expect(solver.checkValue(parsedBoard, 3, 7, 3)).to.be.ok;
+      // Match found. Return false
+      expect(solver.checkValue(parsedBoard, 0, 0, 9)).to.not.be.ok;
+      expect(solver.checkValue(parsedBoard, 3, 7, 1)).to.not.be.ok;
+    });
+  });
+  
+  describe("#solvePuzzle", function(){
+    it("should find a solution to the puzzle passed in", function(){
+      var solution = solver.solvePuzzle(parsedBoard, emptyPositions),
+        expectedSolution = [[ 8,9,5,7,4,2,1,3,6 ],
+                            [ 2,7,1,9,6,3,4,8,5 ],
+                            [ 4,6,3,5,8,1,7,9,2 ],
+                            [ 9,3,4,6,1,7,2,5,8 ],
+                            [ 5,1,7,2,3,8,9,6,4 ],
+                            [ 6,8,2,4,5,9,3,7,1 ],
+                            [ 1,5,9,8,7,4,6,2,3 ],
+                            [ 7,4,6,3,2,5,8,1,9 ],
+                            [ 3,2,8,1,9,6,5,4,7 ]];
+                              
+      expect(solution).to.eql(expectedSolution)
     });
   });
   
